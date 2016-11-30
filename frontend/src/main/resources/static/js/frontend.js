7082369fb4b928a083b0bf75ce84205c001af413
Martin Carlsson <martin.carlsson@omegapoint.se>
2016-11-30 14:02:00 +0100

    Filter on genre and country.

--- frontend/src/main/resources/static/js/frontend.js
+++ frontend/src/main/resources/static/js/frontend.js
@@ -1,18 +1,36 @@
 (function () {
     "use strict";
     angular.module('frontend', [])
-        .controller("moviesController", function ($scope, $http) {
+        .controller("moviesController", function ($scope, $http, $filter) {
         $http.defaults.headers.common.Accept = "application/json";
-        $scope.movies = {Movies: []}
-        $scope.error = {message: ""};
+        $scope.movies = [];
+        $scope.countries = [
+            {name: ''}
+        ];
+        $scope.country = $scope.countries[0];
+        $scope.genres = [
+            {name: ''}
+        ];
+        $scope.genre = $scope.genres[0];
         $scope.select = function(movie) {
-        console.log(movie.Title);
-        movie.selected = !movie.selected;
+            movie.selected = !movie.selected;
+        };
+
+        $scope.filter = function (movie) {
+        return movie.Country.includes($scope.country.name) && movie.Genre.includes($scope.genre.name);
         };
 
         function getMovies() {
         $http.get('/Code Test Data.txt').then(function(result) {
-        $scope.movies = result.data;
+            $scope.movies = result.data.Movies;
+            $scope.movies.forEach(function(movie) {
+                movie.Country.split(',').forEach(function(country) {
+                    $scope.countries.push({name: country});
+                });
+                movie.Genre.split(',').forEach(function(genre) {
+                    $scope.genres.push({name: genre});
+                });
+            });
         });
         }
         getMovies();
